e335e7ba7d1fcf1c59b65667f324e15866801037
Miquel <6692.joan23@fje.edu>
2021-10-08 19:51:53 +0200

    v2 apppropinas js

--- js/code.js
+++ js/code.js
@@ -1,3 +1,13 @@
 function calculo() {
-    alert("hellow");
+    /*alert("hellow");*/
+    let totalCuenta = document.getElementById('total').value;
+    let servicio = document.getElementById('servicio').value;
+    //let servicio = document.getElementByName('servicio')[0].value;
+    // traer el valor nº personas mediante la classe asociada
+    let personas = document.getElementById('personas').value
+        //let servicio = document.getElementByClass('personas')[0].value;
+        /*alert(totalCuenta);
+        alert(servicio);
+        alert(personas);*/
+    console.log('total: ' + totalCuenta + ' servicio: ' + servicio + ' personas: ' + personas)
 }
